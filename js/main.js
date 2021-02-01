f5c940fb882d73183adaa72abeb65dc7782cd1f9
Mikhail Suhaka <msuhaka@gmail.com>
2021-02-01 11:01:24 +0100

    Назначение функций для получения случайных чисел

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const getRandomNumber = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const getRandomInteger = function (min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand.toFixed());
+}
